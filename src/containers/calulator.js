5f56bb13c1cf4bceeec39cb2860a39e9d5fe9aab
Robert Abreu <robertabreu@pursuit.org>
2019-02-05 18:14:46 -0500

    created row containing 123+; added values to buttons; created function to log values

--- src/containers/calulator.js
+++ src/containers/calulator.js
@@ -37,30 +37,39 @@ class Calculator extends React.Component {
         return num;
     }
 
+    logVal = (e) => {
+        console.log(e.target.value)
+    }
+
     render() {
         return (
             <>
                 <div className='row'>
                     <Display className={'text-right py-2 px-3 bg-dark overflow-auto display-2 text-light p-1 col-12'} value={'0'} />
 
-                    <Buttons className='col-3' name={'AC'} />
-                    <Buttons className='col-3' name={'%'} />
-                    <Buttons className='col-3' name={'±'} />
-                    <Buttons className='col-3' name={'÷'} color={'orange'} />
+                    <Buttons className='col-3' name={'AC'} value='AC' onClick={this.logVal} />
+                    <Buttons className='col-3' name={'%'} value='percent' onClick={this.logVal} />
+                    <Buttons className='col-3' name={'±'} value='abs' onClick={this.logVal} />
+                    <Buttons className='col-3' name={'÷'} color={'orange'} value='divide' onClick={this.logVal} />
 
-                    <Buttons className='col-3' name={'7'} />
-                    <Buttons className='col-3' name={'8'} />
-                    <Buttons className='col-3' name={'9'} />
-                    <Buttons className='col-3' name={'x'} color={'orange'} />
+                    <Buttons className='col-3' name={'7'} value='7' onClick={this.logVal} />
+                    <Buttons className='col-3' name={'8'} value='8' onClick={this.logVal} />
+                    <Buttons className='col-3' name={'9'} value='9' onClick={this.logVal} />
+                    <Buttons className='col-3' name={'x'} color={'orange'} value='multiply' onClick={this.logVal} />
 
-                    <Buttons className='col-3' name={'4'} />
-                    <Buttons className='col-3' name={'5'} />
-                    <Buttons className='col-3' name={'6'} />
-                    <Buttons className='col-3' name={'-'} color={'orange'} />
+                    <Buttons className='col-3' name={'4'} value='4' onClick={this.logVal} />
+                    <Buttons className='col-3' name={'5'} value='5' onClick={this.logVal} />
+                    <Buttons className='col-3' name={'6'} value='6' onClick={this.logVal} />
+                    <Buttons className='col-3' name={'-'} color={'orange'} value='subtract' onClick={this.logVal} />
 
-                    <Buttons className='col-6' name={'0'} />
-                    <Buttons className='col-3' name={'.'} />
-                    <Buttons className='col-3' name={'='} color={'orange'} />
+                    <Buttons className='col-3' name={'1'} value='1' onClick={this.logVal} />
+                    <Buttons className='col-3' name={'2'} value='2' onClick={this.logVal} />
+                    <Buttons className='col-3' name={'3'} value='3' onClick={this.logVal} />
+                    <Buttons className='col-3' name={'+'} color={'orange'} value='addition' onClick={this.logVal} />
+
+                    <Buttons className='col-6' name={'0'} value='0' onClick={this.logVal} />
+                    <Buttons className='col-3' name={'.'} value='decimal' onClick={this.logVal} />
+                    <Buttons className='col-3' name={'='} color={'orange'} value='equal' onClick={this.logVal} />
                 </div>
             </>
         )
